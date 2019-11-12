9c21c7cd560aecc0c27d4d1ccc72ea3d7c212b36
traviswhittington88 <traviswhittington@ymail.com>
2019-11-12 16:12:55 -0600

    added logic for all params queried together

--- app.js
+++ app.js
@@ -24,6 +24,33 @@ app.use(function validateBearerToken(req, res, next) {
 
 function handleGetMovie(req, res) {
     const { genre, country, avg_vote } = req.query;
+
+    if(avg_vote && genre && country) {
+        const avg_voteNum = parseFloat(avg_vote)
+        if(Number.isNaN(avg_voteNum)) {
+            res.status(400).send('please provide a numeric value for avg_vote parameter');
+        }
+
+        /*const response = MOVIEDEX
+              .filter(movie => { return movie.avg_vote >= avg_voteNum 
+            }).filter(movie => { return (movie.genre.toLowerCase()).includes(genre.toLowerCase())
+            }).filter(movie => { return (movie.country.toLowerCase()).includes(country.toLowerCase())
+            }) */
+         
+        const avgResponse = MOVIEDEX.filter(movie =>  movie.avg_vote >= avg_voteNum);
+   
+
+        const genreResponse = avgResponse.filter(movie => 
+            (movie.genre.toLowerCase()).includes(genre.toLowerCase()))
+
+        const response = genreResponse.filter(movie => 
+            (movie.country.toLowerCase()).includes(country.toLowerCase()))
+        
+        if(response.length === 0) {
+            res.status(400).send('no movies with that avg_vote, try again')
+        }
+        return res.status(200).json(response)
+    }
     if(genre) {
         const response = MOVIEDEX.filter(movie =>
             (movie.genre.toLowerCase()).includes(genre.toLowerCase()))
@@ -41,7 +68,6 @@ function handleGetMovie(req, res) {
         }
         return res.status(200).json(response)
     }
-    
     if(avg_vote) {
         const avg_voteNum = parseFloat(avg_vote)
 
@@ -53,7 +79,7 @@ function handleGetMovie(req, res) {
             res.status(400).send('no movies with that avg_vote, try again')
         }
         const sortedResponse = response.sort((a, b) => {
-          return a[avg_vote] > b[avg_vote] ? 1 : a[avg_vote] < b[avg_vote] ? -1 : 0;
+          return a[avg_voteNum] > b[avg_voteNum] ? 1 : a[avg_voteNum] < b[avg_voteNum] ? -1 : 0;
         })
         
         return res.status(200).json(sortedResponse);
