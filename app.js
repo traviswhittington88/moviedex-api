c2535acc9dba849ef0e2c4d63edd73537c2ba2a0
traviswhittington88 <traviswhittington@ymail.com>
2019-11-16 08:52:21 -0600

    final commit

--- app.js
+++ app.js
@@ -24,62 +24,37 @@ app.use(function validateBearerToken(req, res, next) {
   })
 
 function handleGetMovie(req, res) {
-    const { genre, country, avg_vote } = req.query;
-    let response = MOVIEDEX;
+  const { genre, country, avg_vote } = req.query;
+  let response = MOVIEDEX;
 
-    function notANumber(num) {
-        if (Number.isNaN(num)) {
-            res.status(400).send('please provide a numeric value for avg_vote parameter');
-        }
-    }
+  function notANumber(num) {
+      if (Number.isNaN(num)) {
+          res.status(400).send('please provide a numeric value for avg_vote parameter');
+      }
+  }
+          
+  if (genre) {
+    response = response.filter(movie => (movie.genre.toLowerCase()).includes(genre.toLowerCase()))
+  } 
+  if(country) {
+    response = response.filter(movie => (movie.country.toLowerCase()).includes(country.toLowerCase()));
+  } 
+            
+  if (avg_vote) {
+    const avg_vote_float = parseFloat(avg_vote)
+    notANumber(avg_vote_float);
+
+    response = response.filter(movie =>  movie.avg_vote >= avg_vote_float)
+                        .sort((a, b) => {
+                          return a.avg_vote > b.avg_vote ? 1 : a.avg_vote < b.avg_vote ? -1 : 0;
+                        })
+  } 
     
-    if (avg_vote && genre && country) {
-      const avg_vote_float = parseFloat(avg_vote)
-      notANumber(avg_vote_float);
-      response = response
-        .filter(movie => movie.avg_vote >= avg_vote_float)
-        .filter(movie => (movie.genre.toLowerCase()).includes(genre.toLowerCase()))
-        .filter(movie => (movie.country.toLowerCase()).includes(country.toLowerCase())) 
-    } else if (avg_vote && genre) {
-        const avg_vote_float = parseFloat(avg_vote)
-        notANumber(avg_vote_float);
-        response = response
-          .filter(movie => movie.avg_vote >= avg_vote_float)
-          .filter(movie => (movie.genre.toLowerCase()).includes(genre.toLowerCase()))
-          .sort((a, b) => {
-              return a.avg_vote > b.avg_vote ? 1 : a.avg_vote < b.avg_vote ? -1 : 0;
-          })
-      } else if (avg_vote && country) {
-          const avg_vote_float = parseFloat(avg_vote)
-          notANumber(avg_vote_float);
-          response = response
-            .filter(movie => movie.avg_vote >= avg_vote_float)
-            .filter(movie => (movie.country.toLowerCase()).includes(country.toLowerCase()))
-            .sort((a, b) => {
-              return a.avg_vote > b.avg_vote ? 1 : a.avg_vote < b.avg_vote ? -1 : 0;
-            })
-        } else if (genre && country ) {
-            response = response.filter(movie => (movie.genre.toLowerCase()).includes(genre.toLowerCase()))
-            .filter(movie => (movie.country.toLowerCase()).includes(country.toLowerCase())) 
-          } else if (genre) {
-              response = response.filter(movie => (movie.genre.toLowerCase()).includes(genre.toLowerCase()))
-            } else if (country) {
-                response = response.filter(movie => (movie.country.toLowerCase()).includes(country.toLowerCase()));
-              } else if (avg_vote) {
-                  const avg_vote_float = parseFloat(avg_vote)
-                  notANumber(avg_vote_float);
+  if(response.length === 0) {
+      res.status(400).send('Oops those movies are not in the db')
+  }
 
-                  response = response.filter(movie =>  movie.avg_vote >= avg_vote_float)
-                                      .sort((a, b) => {
-                                        return a.avg_vote > b.avg_vote ? 1 : a.avg_vote < b.avg_vote ? -1 : 0;
-                                      })
-                } 
-    
-   if(response.length === 0) {
-       res.status(400).send('Oops those movies are not in the db')
-   }
-
-   return res.status(200).json(response);
+  return res.status(200).json(response);
 }
 
 app.get('/movie', handleGetMovie);
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use((error, req, res, next) => {
     }
     res.status(500).json(response)
   })
-  
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)});
